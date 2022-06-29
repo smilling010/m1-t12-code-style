3883bbc5cdef87077621f0e2c69cf323e2f2bb97
Irina <iri-bashkina@yandex.ru>
2022-06-29 17:37:12 +0300

    chandes The opening curly brace is on the same line as the        instructions.         The transfer is made after it.         The closing curly brace remained on the line with the previous instruction.

--- src/calculate_deposit.java
+++ src/calculate_deposit.java
@@ -5,15 +5,17 @@ public class calculate_deposit
 double Calculate_Complex_Percent_Function(double a, double y,int d ) {
        double pay = a * Math.pow((1 + y/ 12), 12 *d);
           return rnd(pay, 2);
-  } double Calculate_Simple_Percent_Function(double doubleAmount,double double_year_rate, int deposit_period) {
+  }
+  double Calculate_Simple_Percent_Function(double doubleAmount,double double_year_rate, int deposit_period) {
          return rnd(doubleAmount+doubleAmount * double_year_rate *deposit_period, 2);
-    } double rnd(double value, int places) {
+    }
+    double rnd(double value, int places) {
        double ScaLe= Math.pow(10, places);
-       return Math.round(value*ScaLe)
-            /ScaLe; }
+            return Math.round(value*ScaLe)
+            /ScaLe;
+    }
 
-    void do_important_job( )
-    {
+    void do_important_job( ) {
       int period;
       int action;
 
@@ -22,21 +24,19 @@ double Calculate_Complex_Percent_Function(double a, double y,int d ) {
       int amount = abcdef.nextInt();
       System.out.println("Введите срок вклада в годах:") ;
       period = abcdef.nextInt( );
-      System.out.println   (   "Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-        action = abcdef.nextInt();
-        double outDoubleVar = 0;
-        if (action ==1) outDoubleVar = Calculate_Simple_Percent_Function(amount, 0.06, period);
-        else if (action == 2)
-            {
-            outDoubleVar = Calculate_Complex_Percent_Function(amount, 0.06, period); }
+      System.out.println   ("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+      action = abcdef.nextInt();
+      double outDoubleVar = 0;
+      if (action ==1) {
+          outDoubleVar = Calculate_Simple_Percent_Function(amount, 0.06, period);
+      } else if (action == 2) {
+            outDoubleVar = Calculate_Complex_Percent_Function(amount, 0.06, period);
+      }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
     }
-public static void main(String[] args)
-    {
+public static void main(String[] args) {
+
         new calculate_deposit().do_important_job();
 }
-
-
-
 
 }
